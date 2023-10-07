6417203ae7edba55aaa8c849f2fb92cff8a7b0b9
SLotman <slotmanrj@yahoo.com.br>
2023-10-07 08:07:51 -0300

    first commit

---
+++ resource.h
@@ -0,0 +1,21 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by Evawin.rc
+//
+#define VERSIONINFO_0                   1
+#define VERSIONINFO_3                   3
+#define VERSIONINFO_4                   4
+#define IDM_SCANLINES                   40003
+#define IDM_SCREEN8                     40004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40005
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
